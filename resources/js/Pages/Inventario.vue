8c51889d924949edb0f14c3d88ba829242168d1b
Mani-9 <manu201119@hotmail.com>
2025-04-23 00:06:04 -0600

    Nav-bar

--- resources/js/Pages/Inventario.vue
+++ resources/js/Pages/Inventario.vue
@@ -2,15 +2,14 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
-
 <template>
 
-    <Head title="Dashboard" />
+    <Head title="Inventario" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                Dashboard
+                Inventario
             </h2>
         </template>
 
@@ -18,7 +17,7 @@ import { Head } from '@inertiajs/vue3';
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        You're logged in!ss
+                        You're logged inss!ss
                     </div>
                 </div>
             </div>
